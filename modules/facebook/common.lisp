4e2e56043bbab5c19af51e973889efadfd71f548
Stas Boukarev <stassats@gmail.com>
2013-04-03 21:13:33 +0400

    Simplify post-scheduled-action.

--- modules/facebook/common.lisp
+++ modules/facebook/common.lisp
@@ -1,189 +1,91 @@
 (in-package :dyb)
 
 (defun post-scheduled-action (action)
-  ;;(break "~A" action)
-  (when (and action
-             (equal (action-status action) "Pending"))
-    (let ((from-user (or (get-channel-user-by-user-name
-                          (get-val action 'from-user-id)
-                          (get-val action 'post-type))
-                         (get-channel-user-by-user-id
-                          (get-val action 'from-user-id)
-                          (get-val action 'post-type)))))
-      (cond ((string-equal (get-val action 'post-type) "Facebook")
-             (cond ((string-equal (get-val action 'action-type) "Post")
-                    (multiple-value-bind (result error-message)
-                        (cond
-                          ((and (blank-p (get-val action 'image-url))
-                                (blank-p (get-val action 'post-url)))
-                           (post-facebook-link-image (get-val action 'from-user-id)
-                                                     (get-val action 'action-content)
-                                                     (or (format-short-url
-                                                          (get-val action 'short-url))
-                                                         (get-val action 'post-url))
-                                                     (get-val action 'image-url)))
+  (with-slots (action-status post-type action-type
+               post-id) action
+    (when (and action
+               (equal action-status "Pending"))
+      (let ((from-user (or (get-channel-user-by-user-name
+                            (get-val action 'from-user-id)
+                            (get-val action 'post-type))
+                           (get-channel-user-by-user-id
+                            (get-val action 'from-user-id)
+                            (get-val action 'post-type)))))
+        (multiple-value-bind (result error-message)
+            (cond ((equal post-type "Facebook")
+                   (cond ((equal action-type "Post")
+                          (cond
+                            ((and (blank-p (get-val action 'image-url))
+                                  (blank-p (get-val action 'post-url)))
+                             (post-facebook-link-image (get-val action 'from-user-id)
+                                                       (get-val action 'action-content)
+                                                       (or (format-short-url
+                                                            (get-val action 'short-url))
+                                                           (get-val action 'post-url))
+                                                       (get-val action 'image-url)))
 
-                          ((blank-p (get-val action 'image-url))
-                           (post-facebook-image (get-val action 'from-user-id)
+                            ((blank-p (get-val action 'image-url))
+                             (post-facebook-image (get-val action 'from-user-id)
+                                                  (get-val action 'action-content)
+                                                  (get-val action 'image-url)))
+                            ((or (blank-p (get-val action 'short-url))
+                                 (blank-p (get-val action 'post-url)))
+                             (post-facebook-url (get-val action 'from-user-id)
                                                 (get-val action 'action-content)
-                                                (get-val action 'image-url)))
-                          ((or (blank-p (get-val action 'short-url))
-                               (blank-p (get-val action 'post-url)))
-                           (post-facebook-url (get-val action 'from-user-id)
-                                              (get-val action 'action-content)
-                                              (or (format-short-url
-                                                   (get-val action 'short-url))
-                                                  (get-val action 'post-url))))
-                          (t
-                           (post-facebook (get-val action 'from-user-id)
-                                          (get-val action 'action-content)))
-                          )
+                                                (or (format-short-url
+                                                     (get-val action 'short-url))
+                                                    (get-val action 'post-url))))
+                            (t
+                             (post-facebook (get-val action 'from-user-id)
+                                            (get-val action 'action-content)))))
+                         ((equal action-type "Comment")
+                          (comment-facebook from-user
+                                            post-id
+                                            ;;  (get-val action 'from-user-id)
+                                            (get-val action 'action-content)))
+                         ((equal action-type "Like")
+                          (facebook-like from-user  post-id
+                                         ;;(get-val action 'from-user-id)
+                                         ))))
 
-                      (when error-message
-
-                        (add-generic-action-log action
-                                                "Error"
-                                                error-message
-                                                "Pending"))
-                      (unless error-message
-                        (add-generic-action-log action
-                                                "Result"
-                                                result
-                                                "Completed"))))
-                   ((string-equal (get-val action 'action-type) "Comment")
-                    (multiple-value-bind (result error-message)
-                        (comment-facebook from-user
-                                          (get-val action 'post-id)
-                                          ;;  (get-val action 'from-user-id)
-                                          (get-val action 'action-content))
-                      (when error-message
-                        (add-generic-action-log action
-                                                "Error"
-                                                error-message
-                                                "Pending"))
-                      (unless error-message
-                        (add-generic-action-log action
-                                                "Result"
-                                                result
-                                                "Completed"))))
-                   ((string-equal (get-val action 'action-type) "Like")
-                    (multiple-value-bind (result error-message)
-                        (facebook-like from-user  (get-val action 'post-id)
-                                       ;;(get-val action 'from-user-id)
-                                       )
-                      (when error-message
-                        (add-generic-action-log action
-                                                "Error"
-                                                error-message
-                                                "Pending"))
-                      (unless error-message
-                        (add-generic-action-log action
-                                                "Result"
-                                                result
-                                                "Completed"))))))
-
-            ((string-equal (get-val action 'post-type) "Twitter")
-
-             (cond ((or (string-equal (get-val action 'action-type) "Tweet")
-                        (string-equal (get-val action 'action-type) "Post"))
-
-                    (multiple-value-bind (result error-message)
-                        (post-twitter
-                         from-user
-                         (get-val action 'action-content)
-                         :image-path (get-val action 'image-url)
-                         :link-url (if (or (blank-p (get-val action 'short-url))
-                                           (blank-p (get-val action 'post-url)))
-                                       (or (format-short-url
-                                            (get-val action 'short-url))
-                                           (get-val action 'post-url))))
-                      (when error-message
-                        (add-generic-action-log action
-                                                "Error"
-                                                error-message
-                                                "Pending"))
-                      (unless error-message
-                        (add-generic-action-log action
-                                                "Result"
-                                                result
-                                                "Completed"))))
-                   ((string-equal (get-val action 'action-type) "Retweet")
-                    (multiple-value-bind (result error-message)
-                        (retweet-twitter
-                         from-user
-                         (get-val action 'post-id))
-                      (when error-message
-                        (add-generic-action-log action
-                                                "Error"
-                                                error-message
-                                                "Pending"))
-                      (unless error-message
-                        (add-generic-action-log action
-                                                "Result"
-                                                result
-                                                "Completed"))))
-                   ((string-equal (get-val action 'action-type) "Reply")
-                    (multiple-value-bind (result error-message)
-                        (reply-twitter
-                         from-user
-                         (get-val action 'action-content)
-                         ;;TODO: Get right user name
-                         (get-val action 'to-user-id)
-                         )
-                      (when error-message
-                        (add-generic-action-log action
-                                                "Error"
-                                                error-message
-                                                "Pending"))
-                      (unless error-message
-                        (add-generic-action-log action
-                                                "Result"
-                                                result
-                                                "Completed"))))
-                   ((string-equal (get-val action 'action-type) "Favourite")
-                    (multiple-value-bind (result error-message)
-                        (favourite-twitter
-                         from-user
-                         (get-val action 'post-id)
-                         )
-                      (when error-message
-                        (add-generic-action-log action
-                                                "Error"
-                                                error-message
-                                                "Pending"))
-                      (unless error-message
-                        (add-generic-action-log action
-                                                "Result"
-                                                result
-                                                "Completed"))))
-                   )
-             )
-            ((string-equal (get-val action 'post-type) "LinkedIn")
-
-             (cond ((string-equal (get-val action 'action-type) "Post")
-
-                    (multiple-value-bind (result error-message)
-                        (if (string-equal (get-val from-user 'profile-type) "Page")
-                            (linkedin-company-share
-                             from-user
-                             (get-val action 'action-content)
-                             :submited-url (or (format-short-url
-                                                (get-val action 'short-url))
-                                               (get-val action 'post-url))
-                             :submitted-image-url (get-val action 'post-url))
-                            (linkedin-share
-                             from-user
-                             (get-val action 'action-content)
-                             :submited-url (or (format-short-url (get-val action 'short-url))
-                                               (get-val action 'post-url))
-                             :submitted-image-url (get-val action 'post-url)))
-                      (when error-message
-                        (add-generic-action-log action
-                                                "Error"
-                                                error-message
-                                                "Pending"))
-                      (unless error-message
-                        (add-generic-action-log action
-                                                "Result"
-                                                result
-                                                "Completed"))))))))))
+                  ((equal post-type "Twitter")
+                   (cond ((or (equal action-type "Tweet")
+                              (equal action-type "Post"))
+                          (post-twitter
+                           from-user
+                           (get-val action 'action-content)
+                           :image-path (get-val action 'image-url)
+                           :link-url (if (or (blank-p (get-val action 'short-url))
+                                             (blank-p (get-val action 'post-url)))
+                                         (or (format-short-url
+                                              (get-val action 'short-url))
+                                             (get-val action 'post-url)))))
+                         ((equal action-type "Retweet")
+                          (retweet-twitter from-user post-id))
+                         ((equal action-type "Reply")
+                          (reply-twitter
+                           from-user
+                           (get-val action 'action-content)
+                           ;;TODO: Get right user name
+                           (get-val action 'to-user-id)))
+                         ((equal action-type "Favourite")
+                          (favourite-twitter from-user post-id))))
+                  ((equal post-type "LinkedIn")
+                   (cond ((equal action-type "Post")
+                          (if (string-equal (get-val from-user 'profile-type) "Page")
+                              (linkedin-company-share
+                               from-user
+                               (get-val action 'action-content)
+                               :submited-url (or (format-short-url
+                                                  (get-val action 'short-url))
+                                                 (get-val action 'post-url))
+                               :submitted-image-url (get-val action 'post-url))
+                              (linkedin-share
+                               from-user
+                               (get-val action 'action-content)
+                               :submited-url (or (format-short-url (get-val action 'short-url))
+                                                 (get-val action 'post-url))
+                               :submitted-image-url (get-val action 'post-url)))))))
+          (if error-message
+              (add-generic-action-log action "Error" error-message "Pending")
+              (add-generic-action-log action "Result" result "Completed")))))))
