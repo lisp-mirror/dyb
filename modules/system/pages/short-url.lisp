63ddc3b47229e89945fccaef86b4a7e7d15ff43d
Stas Boukarev <stassats@gmail.com>
2013-04-09 12:06:53 +0400

    Fix short url handlers.

--- modules/system/pages/short-url.lisp
+++ modules/system/pages/short-url.lisp
@@ -1,31 +1,25 @@
 (in-package :dyb)
 
-(define-easy-handler (shorten-url :uri (lambda (x)
-                                         (alexandria:starts-with-subseq
-                                          "/dyb/s/"
-                                          (script-name x)))
-                                  :for-everyone t) ()
-  (let* ((short (subseq (script-name*) (length "/dyb/s/")))
-         (url (expand-short-url short)))
+(defun handle-short-url (short)
+  (let ((url (expand-short-url short)))
     (cond (url
            (incf (click-count url))
            (persist url)
-           (redirect-external (url url) :host ""))
+           (redirect-external (url url)))
           (t
            (setf (return-code*) +http-not-found+)
            (abort-request-handler)))))
 
 (define-easy-handler (shorten-url :uri (lambda (x)
                                          (alexandria:starts-with-subseq
-                                          "/s/"
+                                          "/dyb/s/"
                                           (script-name x)))
                                   :for-everyone t) ()
-  (let* ((short (subseq (script-name*) (length "/s/")))
-         (url (expand-short-url short)))
-    (cond (url
-           (incf (click-count url))
-           (persist url)
-           (redirect-external (url url) :host ""))
-          (t
-           (setf (return-code*) +http-not-found+)
-           (abort-request-handler)))))
+  (handle-short-url (subseq (script-name*) (length "/dyb/s/"))))
+
+(define-easy-handler (shorten-url-2 :uri (lambda (x)
+                                           (alexandria:starts-with-subseq
+                                            "/s/"
+                                            (script-name x)))
+                                    :for-everyone t) ()
+  (handle-short-url (subseq (script-name*) (length "/s/"))))
